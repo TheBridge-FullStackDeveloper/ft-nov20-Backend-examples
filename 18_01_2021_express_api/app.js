23c640a7ed609508e7d8c3c3603d4143cea201dc
alejandroereyesb <reyesbalejandroe@gmail.com>
2021-01-19 17:00:01 +0100

    add/18_01_2021_express_api

---
+++ 18_01_2021_express_api/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const news = require('./modules/news')
+const app = express() // create server
+const port = 3000
+
+// Middleware
+app.use(express.json()); // para poder leer datos del BODY del post
+
+// Rutas
+// Peticiones GET
+app.get('/api', news.getHome)
+app.get('/api/internacional', news.getInternacional)
+app.get('/api/deportes/:seccion?/:id?', news.getDeportes)
+app.get('/api/politica/:id?', news.getPolitica)
+// Peticion POST
+app.post('/api/crear',news.crearNoticia)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
